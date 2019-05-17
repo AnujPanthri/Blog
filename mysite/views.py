7b37da09eb75c8e94b96e6484af67efdd03d0352
Anuj Panthri <panthrianuj@gmail.com>
2019-05-17 10:08:56 +0530

    changed sign up

--- mysite/views.py
+++ mysite/views.py
@@ -10,7 +10,9 @@ def sign_up_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data.get('password'))
+            user.save()
             return HttpResponseRedirect(reverse('login'))
 
     else:
